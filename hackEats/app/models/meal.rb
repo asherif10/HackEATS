dd52324529ab7a8d3f344f14ebce436885320433
Akash Kejriwal <akash@kejriwal.com>
2017-10-28 16:23:18 -0400

    fixed gemd, json parsing

--- hackEats/app/models/meal.rb
+++ hackEats/app/models/meal.rb
@@ -1,4 +1,5 @@
 require 'http'
+require 'pry'
 
 class Meal < ActiveRecord::Base
 
@@ -10,10 +11,10 @@ class Meal < ActiveRecord::Base
   # basic validation
   validates_presence_of :total_calories
 
-  private 
 
   def image_classify
-  	response = HTTP.get("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=d86e85104db3c496b6ceb8cf3390ee8e69cbdc46&version=2016-05-20", :params => {:url => "https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg"})
+  	response = HTTP.get("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=d86e85104db3c496b6ceb8cf3390ee8e69cbdc46&version=2016-05-20&url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg").to_s
+  	json_response = JSON.parse(response)
   	binding.pry
   end 
 
